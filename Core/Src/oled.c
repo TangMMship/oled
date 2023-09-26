1b796414e9f830b4589e8c8fbd27c440fb9035f5
TangM <1830162050@qq.com>
2023-09-26 21:03:12 +0800

    oled_iic_ssd1315

--- Core/Src/oled.c
+++ Core/Src/oled.c
@@ -4,3 +4,22 @@
 
 #include "oled.h"
 #include "i2c.h"
+
+#define oled_address 0x7a
+
+//send 0x7a 0x00 +0x__ 是发指令  0x__是指令具体内容
+//send 0x7a 0x40 是发数据
+
+
+void oled_sendcmd(uint8_t cmd)
+{
+    uint8_t sendbuffer[2]={0};
+    sendbuffer[0]=0x00;
+    sendbuffer[1]=cmd;
+    HAL_I2C_Master_Transmit(&hi2c1,oled_address,sendbuffer,2,HAL_MAX_DELAY);
+}
+
+void oled_init()
+{
+
+}
